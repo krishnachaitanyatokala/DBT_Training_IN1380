608fbcf0ae52ecff67adab909a69706425ade2a5
krishnat@systechusa.com <krishnat@systechusa.com>
2024-10-07 06:54:07 +0000

    commit

--- models/ex(1)/top5_goals_2011_q1.sql
+++ models/ex(1)/top5_goals_2011_q1.sql
@@ -1,9 +1,25 @@
 {{ config(materialized='table') }}
 
-SELECT t.name as Team_Name, s.year, COUNT(s.G) AS no_of_goals
-FROM HOCKEYDB.HOCKEY.SCORING s
-JOIN HOCKEYDB.HOCKEY.TEAMS t ON s.tmid = t.tmid
-WHERE s.year = 2011
-GROUP BY t.name, s.year
-ORDER BY no_of_goals DESC
-LIMIT 5;
+WITH a AS (
+    SELECT 
+        t.name AS Team_Name, 
+        s.year, 
+        SUM(s.G) AS no_of_goals, 
+        ROW_NUMBER() OVER (PARTITION BY s.year ORDER BY SUM(s.G) DESC) AS rank_1
+    FROM 
+        HOCKEYDB.HOCKEY.SCORING s
+    JOIN 
+        HOCKEYDB.HOCKEY.TEAMS t ON s.tmid = t.tmid
+    WHERE 
+        s.year = 2011
+    GROUP BY 
+        t.name, s.year
+)
+SELECT 
+    Team_Name, 
+    year, 
+    no_of_goals 
+FROM 
+    a 
+WHERE 
+    rank_1 <= 5; 
