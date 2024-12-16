98f08d832ab7a3e7d6ca50e75a8fe1bf250d2f5c
Krishna Chaitnaya Tokala <krishnat@systechusa.com>
2024-12-16 11:52:04 +0000

    final

--- snapshots/cdc/CDC_AWARDSPLAYERS.sql
+++ snapshots/cdc/CDC_AWARDSPLAYERS.sql
@@ -10,16 +10,24 @@
     }}
 
     SELECT
-        PLAYERID,                           
-        AWARD,                               
-        YEAR,                              
-        LGID,                               
-        NOTE,                                 
-        POS,                                  
+        src.PLAYERID,                           
+        src.AWARD,                               
+        src.YEAR,                              
+        src.LGID,                               
+        src.NOTE,                                 
+        src.POS,                                  
         CURRENT_TIMESTAMP AS CREATED_AT,      
         '{{ this.name }}' AS CREATED_BY,      
-        NULL AS VALID_TO,                   
-        CURRENT_TIMESTAMP AS VALID_FROM       
-    FROM 
-        {{ ref('src_stg_AWARDSPLAYERS') }}        
+        CASE 
+        WHEN tgt.PLAYERID IS NULL AND tgt.YEAR IS NULL AND tgt.AWARD IS NULL THEN NULL 
+        ELSE CURRENT_TIMESTAMP 
+    END AS VALID_TO,
+    CURRENT_TIMESTAMP AS VALID_FROM
+FROM 
+    {{ ref('src_stg_AWARDSPLAYERS') }} AS src
+LEFT JOIN 
+    {{ this }} AS tgt
+ON 
+    src.PLAYERID = tgt.PLAYERID AND src.YEAR = tgt.YEAR AND src.AWARD = tgt.AWARD
+
 {% endsnapshot %}
--- snapshots/cdc/CDC_MASTER.sql
+++ snapshots/cdc/CDC_MASTER.sql
@@ -21,9 +21,20 @@
     SRC.DEATHDAY,
     CURRENT_TIMESTAMP as CREATED_AT,  
     'CDC_MASTER' as CREATED_BY, 
-    case when tgt.PLAYERID is null then null else 
-    CURRENT_TIMESTAMP end as LAST_UPDATED_AT 
+    case when tgt.PLAYERID is null then null 
+    when  (tgt.PLAYERID is not null  and   
+    COALESCE(src.FIRSTNAME, '') != COALESCE(tgt.FIRSTNAME, '') OR
+    COALESCE(src.LASTNAME, '') != COALESCE(tgt.LASTNAME, '') OR
+    COALESCE(src.BIRTHYEAR, 0) != COALESCE(tgt.BIRTHYEAR, 0) OR
+    COALESCE(src.BIRTHMON, 0) != COALESCE(tgt.BIRTHMON, 0) OR
+    COALESCE(src.BIRTHDAY, 0) != COALESCE(tgt.BIRTHDAY, 0) OR
+    COALESCE(src.DEATHYEAR, 0) != COALESCE(tgt.DEATHYEAR, 0) OR
+    COALESCE(src.DEATHMON, 0) != COALESCE(tgt.DEATHMON, 0) OR
+    COALESCE(src.DEATHDAY, 0) != COALESCE(tgt.DEATHDAY, 0)) then CURRENT_TIMESTAMP 
+    when   src.PLAYERID is null then CURRENT_TIMESTAMP
+     end as LAST_UPDATED_AT 
 from {{ ref('src_stg_MASTER') }} as src
 left join {{this}} as tgt
 on src.PLAYERID = tgt.PLAYERID
+where tgt.LAST_UPDATED_AT is null
 {%endsnapshot%}
