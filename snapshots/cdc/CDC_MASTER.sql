15e403e748ac17a64f715d86be3d8458c51ded1e
Krishna Chaitnaya Tokala <krishnat@systechusa.com>
2024-12-16 15:39:47 +0000

    final

--- snapshots/cdc/CDC_MASTER.sql
+++ snapshots/cdc/CDC_MASTER.sql
@@ -1,40 +1,44 @@
-
 {% snapshot CDC_MASTER %}
-
     {{
-        config( 
-                
-            unique_key='PLAYERID',                    
+        config(
+            unique_key='PLAYERID',
             strategy='check',
-            check_cols=['FIRSTNAME', 'LASTNAME', 'BIRTHYEAR', 'BIRTHMON', 'BIRTHDAY', 'DEATHYEAR', 'DEATHMON', 'DEATHDAY']
+            check_cols=['FIRSTNAME', 'LASTNAME', 'BIRTHYEAR', 'BIRTHMON', 'BIRTHDAY', 'DEATHYEAR', 'DEATHMON', 'DEATHDAY'],
+            post-hook="""
+                UPDATE {{ this }}
+                SET 
+                    change_type_flag = 'D',
+                    LAST_UPDATED_AT = CURRENT_TIMESTAMP
+                WHERE PLAYERID NOT IN (
+                    SELECT PLAYERID FROM {{ ref('src_stg_MASTER') }}
+                );
+
+                UPDATE {{ this }}
+                SET 
+                    change_type_flag = 'U',
+                    LAST_UPDATED_AT = CURRENT_TIMESTAMP
+                WHERE change_type_flag = 'I'
+                    AND DBT_VALID_TO IS NOT NULL
+            """
         )
     }}
-    select
-    src.PLAYERID,
-    src.FIRSTNAME,
-    SRC.LASTNAME,
-    SRC.BIRTHYEAR,
-    SRC.BIRTHMON,
-    SRC.BIRTHDAY,
-    SRC.DEATHYEAR,
-    SRC.DEATHMON,
-    SRC.DEATHDAY,
-    CURRENT_TIMESTAMP as CREATED_AT,  
-    'CDC_MASTER' as CREATED_BY, 
-    case when tgt.PLAYERID is null then null 
-    when  (tgt.PLAYERID is not null  and   
-    COALESCE(src.FIRSTNAME, '') != COALESCE(tgt.FIRSTNAME, '') OR
-    COALESCE(src.LASTNAME, '') != COALESCE(tgt.LASTNAME, '') OR
-    COALESCE(src.BIRTHYEAR, 0) != COALESCE(tgt.BIRTHYEAR, 0) OR
-    COALESCE(src.BIRTHMON, 0) != COALESCE(tgt.BIRTHMON, 0) OR
-    COALESCE(src.BIRTHDAY, 0) != COALESCE(tgt.BIRTHDAY, 0) OR
-    COALESCE(src.DEATHYEAR, 0) != COALESCE(tgt.DEATHYEAR, 0) OR
-    COALESCE(src.DEATHMON, 0) != COALESCE(tgt.DEATHMON, 0) OR
-    COALESCE(src.DEATHDAY, 0) != COALESCE(tgt.DEATHDAY, 0)) then CURRENT_TIMESTAMP 
-    when   src.PLAYERID is null then CURRENT_TIMESTAMP
-     end as LAST_UPDATED_AT 
-from {{ ref('src_stg_MASTER') }} as src
-left join {{this}} as tgt
-on src.PLAYERID = tgt.PLAYERID
-where tgt.LAST_UPDATED_AT is null
-{%endsnapshot%}
+
+    SELECT
+        src.PLAYERID,
+        src.FIRSTNAME,
+        src.LASTNAME,
+        src.BIRTHYEAR,
+        src.BIRTHMON,
+        src.BIRTHDAY,
+        src.DEATHYEAR,
+        src.DEATHMON,
+        src.DEATHDAY,
+        CURRENT_TIMESTAMP AS CREATED_AT,  
+        'CDC_MASTER' AS CREATED_BY, 
+        NULL AS LAST_UPDATED_AT,
+        'I' AS change_type_flag
+    FROM {{ ref('src_stg_MASTER') }} AS src
+    LEFT JOIN {{ this }} AS tgt
+        ON src.PLAYERID = tgt.PLAYERID
+    WHERE tgt.DBT_VALID_TO IS NULL
+{% endsnapshot %}
